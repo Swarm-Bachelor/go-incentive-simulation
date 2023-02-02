e2d15e65153e7db84d5d6d22d660a39c0fff7c58
Rasmus Øglænd <roglaend@gmail.com>
2023-02-02 11:24:05 +0100

    rename

---
+++ readroutes/read_routes_test.go
@@ -0,0 +1,14 @@
+package readroutes
+
+import (
+	"testing"
+)
+
+func TestReadRoutes(t *testing.T) {
+	allReq, thresholdFails, requestsToBucketZero, rejectedBucketZero, rejectedFirstHop := ReadRoutes("routes_test.json")
+	t.Log("all requests: ", allReq)
+	t.Log("threshold_rejects: ", thresholdFails)
+	t.Log("requests_routed_to_bucket_zero: ", requestsToBucketZero)
+	t.Log("requests_rejected_in_bucket_zero: ", rejectedBucketZero)
+	t.Log("requests_rejected_in_first_hop: ", rejectedFirstHop)
+}
